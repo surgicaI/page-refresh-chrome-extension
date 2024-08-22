e7e0863da6f104b7e71a874cb857baae4dfb715d
Simranjyot Singh Gill <simranjyotsg@adobe.com>
2024-08-22 14:07:07 -0700

    rely on alarms api instead of a always running setInterval loop

--- background.js
+++ background.js
@@ -1,5 +1,14 @@
 chrome.action.onClicked.addListener((tab) => {
-  setInterval(() => {
-    chrome.tabs.reload(tab.id);
-  }, 10 * 60 * 1000); // 10 minutes in milliseconds
+  // Store the active tab's ID
+  const activeTabId = tab.id;
+
+  // Create the alarm, setting its period to 5 minutes (in milliseconds)
+  chrome.alarms.create('refreshAlarm', { periodInMinutes: 5 });
+
+  chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === 'refreshAlarm') {
+      // Refresh the stored tab
+      chrome.tabs.reload(activeTabId);
+    }
+  });
 });
